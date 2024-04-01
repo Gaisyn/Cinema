fb50e396b198d90a65c4380f75b39079cfc10024
Alexandr <agisyn@gmail.com>
2024-04-01 23:34:52 +0300

    cycles

--- js/movie.js
+++ js/movie.js
@@ -21,6 +21,34 @@ for (let i = 0; i < 2; i++) {
   }
 }
 
+// let i = 0;
+// while (i < 2) {
+//   const a = prompt('Один із останіх переглянутих фільмів?', ''),
+//     b = prompt('На скільки оцінете його', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//   } else {
+//     console.log('error');
+//     i--;
+//   }
+//   i++;
+// }
+
+// do {
+//   const a = prompt('Один із останіх переглянутих фільмів?', ''),
+//     b = prompt('На скільки оцінете його', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//   } else {
+//     console.log('error');
+//     i--;
+//   }
+//   i++;
+// }
+// while (i < 2);
+
 if (personalMovieDB.count < 10) {
   console.log("Переглянуто доволі мало фільмів");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
