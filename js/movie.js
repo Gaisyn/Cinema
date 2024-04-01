6e60d7b9230c4ba9ac5bdac8fc456b7f09f3926b
Alexandr <agisyn@gmail.com>
2024-04-01 22:46:49 +0300

    second change movies

--- js/movie.js
+++ js/movie.js
@@ -8,11 +8,27 @@ const personalMovieDB = {
   geners: [],
   privat: false
 };
-const a = prompt('Один із останіх переглянутих фільмів?', ''),
-  b = prompt('На скільки оцінете його', ''),
-  c = prompt('Один із останіх переглянутих фільмів?', ''),
-  d = prompt('На скільки оцінете його', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один із останіх переглянутих фільмів?', ''),
+        b = prompt('На скільки оцінете його', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      console.log('error');
+      i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Переглянуто доволі мало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Ви класичнмй глядач");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Ви кіноман");
+} else {
+  console.log("Сталася помилка");
+}
 
 console.log(personalMovieDB);
